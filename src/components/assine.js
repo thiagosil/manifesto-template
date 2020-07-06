41309686a39c142aa310e3d6981a517b7844f6c9
Thiago Assis <thiagosil@gmail.com>
2020-07-06 13:52:08 -0300

    Styling assine component better

--- src/components/assine.js
+++ src/components/assine.js
@@ -1,68 +1,74 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import SectionContainer from './section-container'
 
-const Container = styled.div`
-  margin: 0 auto;
-  padding: 0 1.0875rem 1rem;
-  padding-top: 20px;
-  max-width: 860px;
-`
-
-const Form = styled.form``
-
-const InputContainer = styled.div`
-  label {
-    flex: none;
-    display: block;
-    width: 125px;
-    font-weight: bold;
-    font-size: 1em;
-  }
-`
-
-const FirstInputContainer = styled.div`
+const FormContainer = styled.div`
   display: flex;
+  align-content: center;
+  justify-content: center;
+`
+
+const FormRow = styled.div`
+  display: flex;
+  margin-top: 1rem;
+`
+
+const FormField = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
 
   label {
-    font-weight: bold;
-    font-size: 1em;
-    margin-left: 10px;
-    display: inline-block;
-    width: 125px;
-    text-align: right;
-  }
-
-  input {
-    margin-left: 10px;
-    font-size: 1em;
+    text-align: left;
   }
 `
+
+const FormColumn = styled.div`
+  display: block;
+
+  & + & {
+    margin-left: 1rem;
+  }
+`
+
+const SignButton = styled.button``
 
 const Assine = () => (
-  <Container id="assine">
+  <SectionContainer id="assine">
     <h1>Assine</h1>
 
-    <div>
-      <Form
+    <FormContainer>
+      <form
         method="post"
         action="https://getform.io/f/83ab3867-aacc-466b-88d2-ae790a98a4d4"
       >
-        <FirstInputContainer>
-          <label>Nome</label>
-          <input type="text" name="name" id="name" />
+        <FormRow>
+          <FormColumn>
+            <FormField>
+              <label>Nome</label>
+              <input type="text" name="name" id="name" />
+            </FormField>
+          </FormColumn>
+          <FormColumn>
+            <FormField>
+              <label>Sobrenome</label>
+              <input type="text" name="last_name" id="last_name" />
+            </FormField>
+          </FormColumn>
+        </FormRow>
+        <FormRow>
+          <FormField>
+            <label>Email</label>
+            <input type="email" name="email" id="email" />
+          </FormField>
+        </FormRow>
 
-          <label>Sobrenome</label>
-          <input type="text" name="last_name" id="last_name" />
-        </FirstInputContainer>
-
-        <InputContainer>
-          <label>Email</label>
-          <input type="email" name="email" id="email" />
-          <button type="submit">Assinar</button>
-        </InputContainer>
-      </Form>
-    </div>
-  </Container>
+        <FormRow>
+          <SignButton type="submit">Assinar</SignButton>
+        </FormRow>
+      </form>
+    </FormContainer>
+  </SectionContainer>
 )
 
 export default Assine
